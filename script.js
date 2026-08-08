0ebab03f6a998d9405f0e10ad5ac207b8d81e50c
Ali Abbas Kazmi <114748253+alikazmidev@users.noreply.github.com>
2026-08-08 05:05:18 +0500

    Updated Homepage & SEO Settings

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(
+  '%cHey, you opened the console.',
+  'color:#9cdcfe;font-family:monospace;font-size:13px;'
+);
+console.log(
+  '%cLet\u2019s talk: hi@aliabbaskazmi.com',
+  'color:#ce9178;font-family:monospace;font-size:13px;'
+);
